5e20c22f0962442edf534085136eaa518ce31734
Cesar Bruneri <ccirillobruneri@gmail.com>
2020-03-30 09:40:35 -0300

    Adicionada pergunta como vc esta?

--- Aula99/Program.cs
+++ Aula99/Program.cs
@@ -9,6 +9,8 @@ namespace Aula99
             Console.WriteLine("Hello World!");
 
             Console.WriteLine("Bom dia!");
+
+            Console.WriteLine("Como vc esta hj?");
         }
     }
 }
